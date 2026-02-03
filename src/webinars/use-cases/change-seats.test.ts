b9fb41fab628c3f5bf1dd3257db69c48d36d6361
Coundoul Adama <adama.coundoul.etu@b13p6.fil.univ-lille.fr>
2026-02-03 16:13:09 +0100

    test: add happy path test for BookSeat use case

--- src/webinars/use-cases/change-seats.test.ts
+++ src/webinars/use-cases/change-seats.test.ts
@@ -1,13 +1,42 @@
-// Tests unitaires
+  // Tests unitaires
+
+    import { testUser } from "src/users/tests/user-seeds";
+    import { InMemoryWebinarRepository } from "../adapters/webinar-repository.in-memory";
+    import { Webinar } from "../entities/webinar.entity";
+    import { ChangeSeats } from "./change-seats";
 
 
-describe('Feature : Change seats', () => {
-  // Initialisation de nos tests, boilerplates...
-  describe('Scenario: Happy path', () => {
-    // Code commun à notre scénario : payload...
-    it('should change the number of seats for a webinar', async () => {
-     // Vérification de la règle métier, condition testée...
-      throw new Error('Not implemented');
+    describe('Feature : Change seats', () => {
+    let webinarRepository: InMemoryWebinarRepository;
+        let useCase: ChangeSeats;
+
+        const webinar = new Webinar({
+            id: 'webinar-id',
+            organizerId: testUser.alice.props.id,
+            title: 'Webinar title',
+            startDate: new Date('2024-01-01T00:00:00Z'),
+            endDate: new Date('2024-01-01T01:00:00Z'),
+            seats: 100,
+        });
+
+        beforeEach(() => {
+            webinarRepository = new InMemoryWebinarRepository([webinar]);
+            useCase = new ChangeSeats(webinarRepository);
+        });
+
+      describe('Scenario: Happy path', () => {
+        const payload = {
+          user: testUser.alice,
+          webinarId: 'webinar-id',
+          seats: 200,
+        };
+        it('should change the number of seats for a webinar', async () => {
+        // ACT
+      await useCase.execute(payload);
+      // ASSERT
+      const updatedWebinar = await webinarRepository.findById('webinar-id');
+      expect(updatedWebinar?.props.seats).toEqual(200);
+        ;
+        });
+      });
     });
-  });
-});
