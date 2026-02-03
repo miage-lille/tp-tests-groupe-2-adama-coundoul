bf19d9b7671f0fda97492c7f21fbe6e3a21a13f0
Coundoul Adama <adama.coundoul.etu@b13p6.fil.univ-lille.fr>
2026-02-03 16:51:50 +0100

    test: add 'webinar does not exist' scenario

--- src/webinars/use-cases/change-seats.test.ts
+++ src/webinars/use-cases/change-seats.test.ts
@@ -1,42 +1,60 @@
-  // Tests unitaires
+// Tests unitaires
 
-    import { testUser } from "src/users/tests/user-seeds";
-    import { InMemoryWebinarRepository } from "../adapters/webinar-repository.in-memory";
-    import { Webinar } from "../entities/webinar.entity";
-    import { ChangeSeats } from "./change-seats";
+import { testUser } from "src/users/tests/user-seeds";
+import { InMemoryWebinarRepository } from "../adapters/webinar-repository.in-memory";
+import { Webinar } from "../entities/webinar.entity";
+import { ChangeSeats } from "./change-seats";
+import { WebinarNotFoundException } from "../exceptions/webinar-not-found";
 
 
-    describe('Feature : Change seats', () => {
-    let webinarRepository: InMemoryWebinarRepository;
-        let useCase: ChangeSeats;
+describe('Feature : Change seats', () => {
+  let webinarRepository: InMemoryWebinarRepository;
+  let useCase: ChangeSeats;
 
-        const webinar = new Webinar({
-            id: 'webinar-id',
-            organizerId: testUser.alice.props.id,
-            title: 'Webinar title',
-            startDate: new Date('2024-01-01T00:00:00Z'),
-            endDate: new Date('2024-01-01T01:00:00Z'),
-            seats: 100,
-        });
+  const webinar = new Webinar({
+    id: 'webinar-id',
+    organizerId: testUser.alice.props.id,
+    title: 'Webinar title',
+    startDate: new Date('2024-01-01T00:00:00Z'),
+    endDate: new Date('2024-01-01T01:00:00Z'),
+    seats: 100,
+  });
 
-        beforeEach(() => {
-            webinarRepository = new InMemoryWebinarRepository([webinar]);
-            useCase = new ChangeSeats(webinarRepository);
-        });
+  beforeEach(() => {
+    webinarRepository = new InMemoryWebinarRepository([webinar]);
+    useCase = new ChangeSeats(webinarRepository);
+  });
 
-      describe('Scenario: Happy path', () => {
-        const payload = {
-          user: testUser.alice,
-          webinarId: 'webinar-id',
-          seats: 200,
-        };
-        it('should change the number of seats for a webinar', async () => {
-        // ACT
+  describe('Scenario: Happy path', () => {
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 200,
+    };
+    it('should change the number of seats for a webinar', async () => {
+      // ACT
       await useCase.execute(payload);
       // ASSERT
       const updatedWebinar = await webinarRepository.findById('webinar-id');
       expect(updatedWebinar?.props.seats).toEqual(200);
-        ;
-        });
-      });
+      ;
     });
+  });
+
+  describe('Scenario: webinar does not exist', () => {
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'unknown-webinar-id',
+      seats: 200,
+    };
+
+    it('should fail', async () => {
+      await expect(useCase.execute(payload)).rejects.toThrow(WebinarNotFoundException);
+
+      const webinar = await webinarRepository.findById('webinar-id');
+      expect(webinar?.props.seats).toEqual(100);
+    });
+  });
+
+
+});
